841630900ca0876807d6392e4a323f79ffc51d17
sjrojanooo <sjrojano@gmail.com>
2021-12-06 21:14:12 -0700

    finishedternary operator challenge. deisgned in if statement using a ternary opertor and calling it from a method. pretty much the same example the instructory presented in the solution. really happy about that.

---
+++ challenges/ternary-operator-challenge/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace ternary_operator_challenge
+{
+    /*
+        Let's create a small application that takes a temperature value as input and checks if the input is an integer or not.
+
+        If the input value is not an integer value, it should print to the console "Not a valid Temperature".
+
+        And if the input value is the valid integer then it should work as mentioned below.
+
+        If input temperature value is <=15 it should write "it is too cold here" to the console.
+
+        If input temperature value is >= 16 and is <=28 it should write "it is ok" to the console.
+
+        If the input temperature value is > 28 it should write "it is hot here" to the console.
+
+        Make sure to use ternary operators and not if statements to check for the three conditions, however you can use if statement for the other conditions like to check if the entered value is a valid integer or not.
+    */
+    class Program
+    {
+
+        static string stateofTemperature; 
+        static void Main(string[] args)
+        {
+
+            //accepting input from the user; 
+            Console.WriteLine("What is the temperature today?"); 
+            int temperature; 
+            //using TryParse to validate the format of the entry; 
+            bool success = int.TryParse(Console.ReadLine(), out temperature); 
+
+            //conditionally output a message based on the entry above; 
+            if(success)
+            {
+                //calling temperature method; 
+                Temperature(temperature); 
+            }
+            else
+            {
+                Console.WriteLine("Not a valid temperature"); 
+            }
+
+            Console.Read(); 
+
+        }
+
+        public static void Temperature(int inputTemp)
+        {
+            stateofTemperature = inputTemp <= 15 ? 
+            //true
+            "it is too cold here":  
+            //false
+            (inputTemp <=28 && inputTemp >=16) ? 
+            //true
+            "it is ok": 
+            //false
+            inputTemp > 28 ? 
+            //true 
+            "it is too cold" : 
+            //false
+            ""; 
+            Console.WriteLine(stateofTemperature); 
+        }
+    }
+}
